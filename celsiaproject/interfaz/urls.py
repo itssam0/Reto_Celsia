d2447fb8de1ad3f6aff89f4fea7c7ca6954d4e09
Sara Isabel Ortiz <siortizh@eafit.edu.co>
2024-04-14 07:57:29 -0500

    direccionamiento urls

--- celsiaproject/interfaz/urls.py
+++ celsiaproject/interfaz/urls.py
@@ -1,22 +1,21 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
-from .views import CartView, CartRemoveAllView, ImageViewFactory, ImageLocalStorage, ImageViewNoDI
-
+from .views import HomeCelsiaPageView, detallesCelsiaPageView, informeCelsiaPageView, anomaliasCelsiaPageView
+from .views import HomeClientePageView, consumoClientePageView, estadisticasClientePageView, nosotrosClientePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', ContactPageView.as_view(), name='contact'),
-    path('products/', ProductIndexView.as_view(), name='index'),
-    path('create/', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>/', ProductShowView.as_view(), name='show'),
-    path('cart/', CartView.as_view(), name='cart_index'),
-    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
-    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
-    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
-    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
-    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
+
+    ## urls vista celsia
+    path('homeCelsia/', HomeCelsiaPageView.as_view(), name='home_celsia'),
+    path('detalles/', detallesCelsiaPageView.as_view(), name='detalles_celsia'),
+    path('informe/', informeCelsiaPageView.as_view(), name='informe_celsia'),
+    path('anomalias/', anomaliasCelsiaPageView.as_view(), name='anomalias_celsia'),
+
+    ## urls vista cliente
+    path('homeCliente/', HomeClientePageView.as_view(), name='home_cliente'),
+    path('consumo/', consumoClientePageView.as_view(), name='consumo_cliente'),
+    path('estadisticasCliente/', estadisticasClientePageView.as_view(), name='estadisticas_cliente'),
+    path('nosotros/', nosotrosClientePageView.as_view(), name='nosotros'),
+
 ]
 
                         
--- celsiaproject/interfaz/views.py
+++ celsiaproject/interfaz/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
 
-def home(request):
-    return render(request, 'homeClient.html')
+## Vistas de la empresa
+class HomeCelsiaPageView(TemplateView):
+    template_name = 'vistaCelsia/homeCelsia.html'
+
+class detallesCelsiaPageView(TemplateView):
+    template_name = 'vistaCelsia/detalles.html'
+
+class informeCelsiaPageView(TemplateView):
+    template_name = 'vistaCelsia/informe.html'
+
+class anomaliasCelsiaPageView(TemplateView):
+    template_name = 'vistaCelsia/anomalias.html'
+
+
+## Vistas del cliente
+class HomeClientePageView(TemplateView):
+    template_name = 'vistaCliente/homeCliente.html'
+
+class consumoClientePageView(TemplateView):
+    template_name = 'vistaCliente/consumo.html'
+
+class estadisticasClientePageView(TemplateView):
+    template_name = 'vistaCliente/estadisticas.html'
+
+class nosotrosClientePageView(TemplateView):
+    template_name = 'vistaCliente/nosotros.html'
+
