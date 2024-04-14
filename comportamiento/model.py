5996d2453fb13e38635387da578afef10334c73a
Samuel Acosta Aristizábal <88010033+itssam0@users.noreply.github.com>
2024-04-14 09:56:34 -0500

    Update model.py

--- comportamiento/model.py
+++ comportamiento/model.py
@@ -1,10 +1,12 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import Dense, Dropout
 
-def create_model(input_shape):
+def create_model(input_dim):
     model = Sequential([
-        LSTM(50, return_sequences=True, input_shape=input_shape),
-        LSTM(50),
+        Dense(68, activation='relu', input_shape=(input_dim,)),  # Asegúrate de que input_dim refleje el nuevo número de características
+        Dropout(0.2),
+        Dense(68, activation='relu'),
+        Dropout(0.2),
         Dense(8)  # 8 targets to predict
     ])
     model.compile(optimizer='adam', loss='mean_squared_error')
