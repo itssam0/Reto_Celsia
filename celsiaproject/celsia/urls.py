853876547faab7df6338ac826d1d7e7faa78ab20
Sara Isabel Ortiz <siortizh@eafit.edu.co>
2024-04-14 01:28:39 -0500

    app interfaz

--- celsiaproject/celsia/urls.py
+++ celsiaproject/celsia/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from interfaz import views as interfazViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', interfazViews.home)
 ]
